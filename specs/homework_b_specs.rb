33627912943123c542ab273ff4084b2d75e93ce3
John Duncan <john@johnduncanphoto.com>
2017-10-02 19:41:12 +0100

    assert coach name passed

--- specs/homework_b_specs.rb
+++ specs/homework_b_specs.rb
@@ -13,7 +13,13 @@ class TestHomework < MiniTest::Test
     assert_equal('Ultimate Team', @team.name)
   end
 
+  def test_players
+    assert_equal(['Ian','Frank','Edmond'], @team.players)
+  end
 
+  def test_coach
+    assert_equal('Profesional Peter', @team.coach)
+  end
 
 
 
